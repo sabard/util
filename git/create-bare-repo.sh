309b091962ac6795d8e425c62f3efb3dad7f9f79
Sabar Dasgupta <s@bardasgupta.com>
2024-02-25 15:21:01 -0800

    use default values

--- git/create-bare-repo.sh
+++ git/create-bare-repo.sh
@@ -1,3 +1,6 @@
-git clone --bare https://github.com/bil/licorice licorice.git
-cd licorice.git
+REPO="${1:-https://github.com/bil/licorice}"
+OUTPUT_FOLDER="${2:-/Users/sabard/dev/bil/licorice.git}"
+
+git clone --bare $REPO $OUTPUT_FOLDER
+cd $OUTPUT_FOLDER
 git update-server-info
